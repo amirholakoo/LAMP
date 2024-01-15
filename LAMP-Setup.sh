a33a6490ff9589bfcce4a328e8b1d657b49e3d36
amirholakoo <amirholakoo@gmail.com>
2024-01-15 05:13:40 -0800

    Update LAMP-Setup.sh

--- LAMP-Setup.sh
+++ LAMP-Setup.sh
@@ -104,7 +104,7 @@
 
  # Set Permissions 
  sudo chown -R admin:www-data /var/www/html/ 
- sudo chmod -R 770 /var/www/html/ 
+ sudo chmod -R 775 /var/www/html/ 
  ls -al  
 
  # Open MySQL to add user
